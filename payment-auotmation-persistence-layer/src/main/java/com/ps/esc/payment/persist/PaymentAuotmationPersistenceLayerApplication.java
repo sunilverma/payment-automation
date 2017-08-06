9d21a7a6efa3887b6b6aded61bd4606c4134005e
sunil <sunilkrverma@gmail.com>
2017-08-06 12:01:40 -0400

    first commit.

--- payment-auotmation-persistence-layer/src/main/java/com/ps/esc/payment/persist/PaymentAuotmationPersistenceLayerApplication.java
+++ payment-auotmation-persistence-layer/src/main/java/com/ps/esc/payment/persist/PaymentAuotmationPersistenceLayerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PaymentAuotmationPersistenceLayerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World :::::::::");
 		SpringApplication.run(PaymentAuotmationPersistenceLayerApplication.class, args);
 	}
 }
